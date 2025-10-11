b2ea695a2e64930a5dd3f6916ee6672b191b3ab6
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-11 17:39:50 +0100

    add /health route

--- app/app.py
+++ app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return f'Hello from this Kubernetes Scalable APP 🚀 - Created by MGhaith - <a href="https://github.com/MGhaith">https://github.com/MGhaith</a>'
 
+@app.route("/health")
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
