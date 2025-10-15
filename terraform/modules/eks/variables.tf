6f25679e2c759ca2c9be8c9b8305b943fbf459c8
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-15 22:59:28 +0100

    add public subnets variable

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_id" {
   type = string
   description = "ID of the VPC where the EKS cluster will be created"
 }
+
+variable "public_subnets" {
+  type = list(string)
+  description = "List of public subnet IDs"
+}
