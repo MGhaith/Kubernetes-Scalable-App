39928c596ddd08854ae593b4ec6c18b8f6761fa7
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-16 14:10:28 +0100

    removed vpc_id variable

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -1,8 +1,3 @@
-variable "vpc_id" {
-  type = string
-  description = "ID of the VPC where the EKS cluster will be created"
-}
-
 variable "public_subnets" {
   type = list(string)
   description = "List of public subnet IDs"
