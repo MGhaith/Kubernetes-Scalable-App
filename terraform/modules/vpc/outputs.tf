7150343f30d06ff6b207760c358ad0a10879c89c
Ghaith Magherbi <retr0creativegm@gmail.com>
2025-10-16 13:58:28 +0100

    modified vpc outputs

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -3,9 +3,9 @@ output "vpc_id" {
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+  value = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 }
